b089010f204d0ba95a457404df40492798b1d6a2
agutierrezeye <109681476+agutierrezeye@users.noreply.github.com>
2022-08-17 10:34:57 -0500

    v2.0

---
+++ videoyoutube.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
+import time 
+import datetime
+import socket
+driver = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
+
+myTime = str(time.strftime(("%d-%m-%Y %H:%M:%S")))
+
+def create_web_driver():
+    driver = webdriver.Chrome()
+
+    driver.set_window_position(0, 0)
+    driver.set_window_size(800, 600)
+    return driver
+
+
+driver = create_web_driver()
+driver.get("https://www.youtube.com/watch?v=jfKfPfyJRdk%22")
+
+
+time.sleep(3)
+
+cookies_banner = driver.find_element(By.CLASS_NAME,"html5-video-container")
+actionChains = ActionChains(driver)
+
+actionChains.context_click(cookies_banner).perform()
+
+#estadistica = driver.find_element(By.CLASS_NAME,"ytp-popup ytp-contextmenu")
+estadistica = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div[6]")
+estadistica.click()
+
+UP = '\033[1A'
+CLEAR = '\x1b[2K'
+
+# file = open('data.txt', 'a+')
+
+while True:
+    file = open('log.txt', 'a+')
+
+    print(f"Host:\t\t\t{socket.gethostbyname(socket.gethostname())}\n")
+    res = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[3]/span").text
+    spd = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[9]/span/span[2]").text
+    nac = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[10]/span/span[2]").text
+    bff = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[11]/span/span[2]").text
+    lila = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[12]/span/span[2]").text
+    date = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[16]/span").text
+    print(f"Resolution:\t\t{res}\nSpeed:\t\t\t{spd}\nNetwork Activity:\t{nac}\nBuffer:\t\t\t{bff}\nLive Latency:\t\t{lila}\nFecha:\t\t\t{date}\n")
+    #pr = f"Speed:\t\t\t{spd}\nNetwork Activity:\t{nac}\nBuffer:\t\t\t{bff}\nLive Latency:\t\t{lila}\n"
+    #print(pr)
+    #print(f"{UP}Speed:\t\t\t{spd}{CLEAR}\n{UP}Network Activity:\t{nac}{CLEAR}\n{UP}Buffer:\t\t\t{bff}{CLEAR}\n{UP}Live Latency:\t\t{lila}{CLEAR}\n")
+    file.write(myTime+" "+socket.gethostbyname(socket.gethostname())+" "+res+" "+spd+" "+nac+" "+bff +" "+lila +"\n")
+    file.close()
+    time.sleep(1)
