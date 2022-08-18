1657d7869242cc79b6cdf7ea1ee5e729810e6336
agutierrezeye <109681476+agutierrezeye@users.noreply.github.com>
2022-08-18 16:17:29 -0500

    v4.0

--- videoyoutube.py
+++ videoyoutube.py
@@ -9,8 +9,6 @@ import socket
 
 # driver = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
 
-myTime = str(time.strftime(("%d-%m-%Y %H:%M:%S")))
-
 def create_web_driver():
     driver = webdriver.Chrome()
 
@@ -40,7 +38,8 @@ CLEAR = '\x1b[2K'
 # file = open('data.txt', 'a+')
 
 while True:
-    file = open('log.txt', 'a+')
+    myTime = str(time.strftime(("%d-%m-%Y %H:%M:%S")))
+    # file = open('log.txt', 'a+')
 
     print(f"Host:\t\t\t{socket.gethostbyname(socket.gethostname())}\n")
     res = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[3]/span").text
@@ -49,19 +48,19 @@ while True:
     bff = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[11]/span/span[2]").text
     lila = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[12]/span/span[2]").text
     date = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[16]/span").text
-    print(f"Resolution:\t\t{res}\nSpeed:\t\t\t{spd}\nNetwork Activity:\t{nac}\nBuffer:\t\t\t{bff}\nLive Latency:\t\t{lila}\nFecha:\t\t\t{date}\n")
+    printConsole = print(f"Resolution:\t\t{res}\nSpeed:\t\t\t{spd}\nNetwork Activity:\t{nac}\nBuffer:\t\t\t{bff}\nLive Latency:\t\t{lila}\nFecha:\t\t\t{date}\n")
     #pr = f"Speed:\t\t\t{spd}\nNetwork Activity:\t{nac}\nBuffer:\t\t\t{bff}\nLive Latency:\t\t{lila}\n"
     #print(pr)
     #print(f"{UP}Speed:\t\t\t{spd}{CLEAR}\n{UP}Network Activity:\t{nac}{CLEAR}\n{UP}Buffer:\t\t\t{bff}{CLEAR}\n{UP}Live Latency:\t\t{lila}{CLEAR}\n")
     
-    st = speedtest.Speedtest() 
-    download_speed = st.download()
-    upload_speed = st.upload()
+    # st = speedtest.Speedtest() 
+    # download_speed = st.download()
+    # upload_speed = st.upload()
     
     # print('Download Speed: {:5.2f} Mb'.format(download_speed/(1024*1024)))
     # # print('Upload Speed: {:5.2f} Mb'.format(upload_speed/(1024*1024)))
-    
-    file.write(myTime+" "+socket.gethostbyname(socket.gethostname())+" "+res+" "+spd+" "+nac+" "+bff +" "+lila +' {:5.2f} '.format(download_speed/(1024*1024))+'{:5.2f}' .format(upload_speed/(1024*1024)))
-    
-    file.close()
+    # file = open('log.txt', 'a+')
+    global youtubeStatistics
+    youtubeStatistics = (socket.gethostbyname(socket.gethostname())+" "+res+" "+spd+" "+nac+" "+bff +" "+lila)
+    # file.close()
     time.sleep(1)
