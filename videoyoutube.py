77af87f130625f5de3595f9c820868cd4dded393
agutierrezeye <109681476+agutierrezeye@users.noreply.github.com>
2022-08-24 08:44:55 -0500

    4k, no ads and JSON v1.0

--- videoyoutube.py
+++ videoyoutube.py
@@ -1,16 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
-import speedtest
-
-import time 
-import datetime
-import socket
 import json
+import socket
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
 
 # driver = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
 
 def create_web_driver():
+    # chrome_options = Options()
+    # chrome_options.add_extension(path.abspath('adblock_plus.crx'))
+    # driver = webdriver.Chrome(chrome_options=chrome_options)
     driver = webdriver.Chrome()
 
     driver.set_window_position(0, 0)
@@ -18,19 +19,30 @@ def create_web_driver():
     return driver
 
 driver = create_web_driver()
-driver.get("https://www.youtube.com/watch?v=_-WWwNt8AzU")
-
-
-time.sleep(3)
+driver.get("https://www.youtube.com/watch_popup?v=_-WWwNt8AzU")
+# driver.switch_to.window(driver.window_handles[1])
+# driver.close()
+# driver.switch_to.window(driver.window_handles[0])
+# time.sleep(20)
 
 cookies_banner = driver.find_element(By.CLASS_NAME,"html5-video-container")
+
+play = driver.find_element(By.CLASS_NAME, "ytp-large-play-button").click()
+mute = driver.find_element(By.CLASS_NAME, "ytp-mute-button").click()
+time.sleep(5)
+
+quality = driver.find_element(By.CLASS_NAME, "ytp-settings-button").click()
+time.sleep(2)
+quality = driver.find_element(By.XPATH, "//div[@class='ytp-panel-menu']/div[3]").click()
+time.sleep(2)
+quality = driver.find_element(By.XPATH, '//span[text()="2160p"]').click()
+time.sleep(2)
+
 actionChains = ActionChains(driver)
-ban = driver.find_element(By.CLASS_NAME, "ytp-play-button")
 actionChains.context_click(cookies_banner).perform()
 
-
-#estadistica = driver.find_element(By.CLASS_NAME,"ytp-popup ytp-contextmenu")
-estadistica = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div[6]")
+# estadistica = driver.find_element(By.CLASS_NAME,"ytp-popup ytp-contextmenu")
+estadistica = driver.find_element(By.XPATH, "//div[@class='ytp-panel-menu']/div[7]")
 estadistica.click()
 
 # UP = '\033[1A'
@@ -41,43 +53,57 @@ estadistica.click()
 while True:
     myTime = str(time.strftime(("%d-%m-%Y %H:%M:%S")))
     fileLog = open('log.txt', 'a+')
-    fileJSON = open('log.json', 'a+')
+    fileJSONData = open('log.json', "w+")
 
     print(f"Host:\t\t\t{socket.gethostbyname(socket.gethostname())}\n")
-    res = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[3]/span").text
-    spd = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[9]/span/span[2]").text
-    nac = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[10]/span/span[2]").text
-    bff = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[11]/span/span[2]").text
-    lila = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[12]/span/span[2]").text
-    date = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[23]/div/div[16]/span").text
-    print(f"Resolution:\t\t{res}\nSpeed:\t\t\t{spd}\nNetwork Activity:\t{nac}\nBuffer:\t\t\t{bff}\nLive Latency:\t\t{lila}\nFecha:\t\t\t{date}\n")
-    
-    #pr = f"Speed:\t\t\t{spd}\nNetwork Activity:\t{nac}\nBuffer:\t\t\t{bff}\nLive Latency:\t\t{lila}\n"
+    listStats = driver.find_elements(By.XPATH, "//div[@class='html5-video-info-panel']/div/div[not(@style='display: none;')]/span")
+    resolution = listStats[2].text
+    speed = listStats[5].find_element(By.XPATH, "./span[2]").text
+    network = listStats[6].find_element(By.XPATH, "./span[2]").text
+    buffer = listStats[7].find_element(By.XPATH, "./span[2]").text
+    latency = listStats[8].find_element(By.XPATH, "./span[2]").text
+    date = listStats[11].text
+    print(f"Resolution:\t\t{resolution}\nSpeed:\t\t\t{speed}\nNetwork Activity:\t{network}\nBuffer:\t\t\t{buffer}\nLive Latency:\t\t{latency}\nFecha:\t\t\t{date}\n")
+
+    #pr = f"Speed:\t\t\t{speed}\nNetwork Activity:\t{network}\nBuffer:\t\t\t{buffer}\nLive Latency:\t\t{latency}\n"
     #print(pr)
-    #print(f"{UP}Speed:\t\t\t{spd}{CLEAR}\n{UP}Network Activity:\t{nac}{CLEAR}\n{UP}Buffer:\t\t\t{bff}{CLEAR}\n{UP}Live Latency:\t\t{lila}{CLEAR}\n")
-    
-    # st = speedtest.Speedtest() 
+    #print(f"{UP}Speed:\t\t\t{speed}{CLEAR}\n{UP}Network Activity:\t{network}{CLEAR}\n{UP}Buffer:\t\t\t{buffer}{CLEAR}\n{UP}Live Latency:\t\t{latency}{CLEAR}\n")
+
+    # st = speedtest.Speedtest()
     # download_speed = st.download()
     # upload_speed = st.upload()
-    
-    
+
     # print('Download Speed: {:5.2f} Mb'.format(download_speed/(1024*1024)))
-    # # print('Upload Speed: {:5.2f} Mb'.format(upload_speed/(1024*1024)))
-    
-    fileLog.write(myTime+" "+socket.gethostbyname(socket.gethostname())+" "+res+" "+spd+" "+nac+" "+bff +" "+lila+"\n")
+    # print('Upload Speed: {:5.2f} Mb'.format(upload_speed/(1024*1024)))
+
+    fileLog.write(myTime+" "+socket.gethostbyname(socket.gethostname())+" "+resolution+" "+speed+" "+network+" "+buffer +" "+latency +' {:5.2f} '.format(0/(1024*1024))+'{:5.2f}' .format(0/(1024*1024))+"\n")
     fileLog.close()
 
     arrayJSON = [
-        {'Fecha':myTime},
-        {'Host':socket.gethostbyname(socket.gethostname())},
-        {'Resolution':res},
-        {'Speed':spd},
-        {'Network Activity':nac},
-        {'}Buffer':bff},
-        {'Latency':lila}
+        {'Fecha':myTime,
+        'Host':socket.gethostbyname(socket.gethostname()),
+        'Resolution':resolution,
+        'Speed':speed,
+        'Network Activity':network,
+        'Buffer':buffer,
+        'Latency':latency,
+        'Download SpeedTest':' {:5.2f} '.format(0/(1024*1024)),
+        'Upload SpeedTest':'{:5.2f}' .format(0/(1024*1024))}
     ]
-    
-    jsonString = json.dumps(arrayJSON)
-    fileJSON.write('{"prtg": { "result": '+jsonString+"}}\n")
 
-    time.sleep(30)
+    
+    # fileJSONSize = len(fileJSONData.read())
+    # fileJSONData = open('log.json', "r")
+
+    # if fileJSONSize > 0:
+    #     dataJSON = json.load(fileJSONData)
+    # else:
+    #     dataJSON = ""
+
+    # arrayJSON = dataJSON["prtg"]["result"] + arrayJSON
+
+    jsonString = json.dumps(arrayJSON)
+    # fileJSON = open('log.json', 'w')
+    fileJSONData.write('{"prtg": { "result": '+jsonString+"}}\n")
+
+    time.sleep(1)
